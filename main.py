3545daf55ef2f2ab74b2a33e58458d9b91b664b2
Łukasz <smietki94@gmail.com>
2022-11-25 00:43:02 +0100

    Add app description, image and links

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas
 
 streamlit.set_page_config(layout='wide')
 
-col1, col2 = streamlit.columns(2)
+col1, col2 = streamlit.columns([0.8, 2])
 
 with col1:
-    streamlit.image('images/photo.jpg', width=300)
+    streamlit.image('images/photo.jpg', width=300,)
 
 with col2:
     streamlit.title('Łukasz Gurgul')
@@ -23,12 +23,20 @@ streamlit.write(text)
 
 df = pandas.read_csv('data.csv', sep=';')
 
-col3, col4 = streamlit.columns(2)
+col3, empty_column, col4 = streamlit.columns([1.5, 0.5, 1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
-        streamlit.header(row['Title'])
+        streamlit.header(row['title'])
+        streamlit.write(row['description'])
+        imagepath = 'images/' + row['image']
+        streamlit.image(imagepath)
+        streamlit.write(f'[Source Code]({row["url"]})')
 
 with col4:
     for index, row in df[10:].iterrows():
-        streamlit.header(row['Title'])
+        streamlit.header(row['title'])
+        streamlit.write(row['description'])
+        imagepath = 'images/' + row['image']
+        streamlit.image(imagepath)
+        streamlit.write(f'[Source Code]({row["url"]})')
